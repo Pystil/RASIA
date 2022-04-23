1d722d0a6f95f3bd1bc134209fb12072fb712e15
Demetrius <46008167+Pystil@users.noreply.github.com>
2022-04-23 05:23:08 +0300

    Alpha1
    -Blank Project has been created

---
+++ RASIAGame/Source/RASIAGame/RASIAGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RASIAGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RASIAGame, "RASIAGame" );
---
+++ RASIAGame/Source/RASIAGame/RASIAGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RASIAGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RASIAGAME_API ARASIAGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
